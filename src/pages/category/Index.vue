595de35c9858c5624dc0be6b3f19a042f38180da
Nur <fawel99@gmail.com>
2024-01-20 15:10:52 +0600

    Added new item button

--- src/pages/category/Index.vue
+++ src/pages/category/Index.vue
@@ -4,6 +4,12 @@
         <div class="table-responsive">
             <table class="table table-bordered text-center">
                 <thead>
+                    <tr>
+                        <td colspan="5">
+                            <router-link :to="{ name: 'admin-category-add' }" type="button"
+                                class="btn btn-primary btn-lg">New Category</router-link>
+                        </td>
+                    </tr>
                     <tr class="bg-light-gray">
                         <th class="text-uppercase">S/L</th>
                         <th class="text-uppercase">Name</th>
@@ -16,19 +22,29 @@
                     <tr v-for="(category, index ) in catObj.data" :key="category.id">
                         <td class="align-middle">{{ index + 1 }}</td>
                         <td> {{ category.name }} </td>
-                        <td> {{ category.children.length }} </td>
-                        <td> {{ category.items.length }} </td>
                         <td>
-                            <router-link :to="{ name: 'admin-all-category', params: { categoryId: category.id } }"
-                                type="button" class="btn btn-outline-primary btn-sm">New Item</router-link>
+
+                            <router-link v-if="category.children.length"
+                                :to="{ name: 'admin-all-category', params: { categoryId: category.id } }" type="button"
+                                class="btn btn-info btn-md"> {{ category.children.length }} Sub
+                                Category</router-link>
+                        </td>
+                        <td>
+                            <router-link v-if="category.items.length"
+                                :to="{ name: 'admin-all-item', params: { categoryId: category.id } }" type="button"
+                                class="btn btn-outline-primary btn-sm"> {{ category.items.length }} Items </router-link>
+                        </td>
+                        <td>
+
 
                             <router-link v-if="!category.items.length"
                                 :to="{ name: 'admin-category-add', params: { categoryId: category.id } }" type="button"
                                 class="btn btn-outline-primary btn-sm">New Sub Category</router-link>
 
-                            <router-link v-if="category.children.length"
-                                :to="{ name: 'admin-all-category', params: { categoryId: category.id } }" type="button"
-                                class="btn btn-outline-primary btn-sm">Sub Category</router-link>
+                            <router-link v-if="!category.children.length" :to="{ name: 'admin-item-add', params: { categoryId: category.id } }"
+                                type="button" class="btn btn-outline-primary btn-sm">New Item</router-link>
+
+
                         </td>
                     </tr>
                 </tbody>
